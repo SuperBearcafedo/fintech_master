6ab7348c737e7b8bfe1544e24b728cb7e0acafd4
Superdo <sijiexiongmore@outlook.com>
2022-11-24 22:59:22 +0800

    [20221124]章节文件整体框架改为 类 进行加工。

--- chapter_format.py
+++ chapter_format.py
@@ -10,71 +10,76 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_UNDERLINE
 from datetime import datetime
 
 
-def create_document_object(create_path, add_file_name):
-    create_path = create_path + "\\" + add_file_name
-    document_object = Document(create_path)
-    return document_object
+class Chapter:  # 类采用Pascal命名风格
+    """
+    目前缺乏对类使用的说明：后续进行补充
+    方法可以在该类下继续添加，逐渐丰富
+    """
+    def __init__(self, create_path, add_file_name, title_name, chapter_name):
+        self.document_object = Document()  # 创建文件对象
+        self.create_path = create_path
+        self.add_file_name = add_file_name
+        self.tn = title_name
+        self.cn = chapter_name
 
+    def createTitle(self):  # 小驼峰命名法：局部使用
+        self.existChapterFile()
+        # 创建文件基本对象信息
+        run_heading = self.document_object.add_heading("", level=1)
+        run = run_heading.add_run(self.tn)  # 以追加段落格式方便后续修改
+        run.font.underline = False  # 不需要添加下划线
+        run_heading.paragraph_format.space_before = Pt(0)  # 设置段前
+        run_heading.paragraph_format.space_after = Pt(0)  # 设置段后
+        run_heading.paragraph_format.line_spacing = 1.5  # 设置行间距
+        run.font.size = Pt(20)  # 设置1级标题文字的大小为“小四” 为12磅
+        run.font.name = "Times New Roman"  # 设置英文字体
+        run._element.rPr.rFonts.set(qn('w:eastAsia'), u"宋体")  # 设置中文字体
+        run.font.color.rgb = RGBColor(0, 0, 0)  # 设置颜色
 
-def create_title(document_object, charpter_name):
+        # 创建内容
+        content = self.document_object.add_paragraph()
+        content.paragraph_format.space_before = Pt(10)  # 设置段前 0 磅
+        content.paragraph_format.space_after = Pt(10)  # 设置段后 0 磅
+        content.paragraph_format.line_spacing = 1.5  # 设置行间距为 1.5倍
+        content.paragraph_format.first_line_indent = Inches(0)  # 段落首行缩进为 0.5英寸
+        content.paragraph_format.first_line_indent = Inches(0)  # 相当于小四两个字符的缩进
+        content_run = content.add_run(self.cn)
+        content_run.bold = False  # 加粗
+        content_run.font.size = Pt(16)
+        content_run.font.name = "Times New Roman"  # 设置英文字体
+        content_run._element.rPr.rFonts.set(qn('w:eastAsia'), u"宋体")  # 设置中文字体
+        content_run.font.color.rgb = RGBColor(0, 0, 0)  # 设置颜色
 
-    return
+        # 创建更新时间，添加时间
+        header_section = self.document_object.sections[0]
+        header_pos = header_section.header
+        header_para = header_pos.paragraphs[0]
+        current_date_and_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+        header_para.text = "该章节编写于:" + str(current_date_and_time)
+
+        self.saveChapter()
+
+        return
+
+    def saveChapter(self):
+        save_path = os.path.join(self.create_path, self.add_file_name)
+        self.document_object.save(save_path)  # 创建文件保存地址
+
+    def existChapterFile(self):
+        if not os.path.exists(self.create_path):
+            print("章节创建文件夹路径不存在，创建目录: {}".format(self.create_path))
+            os.mkdir(create_path)
 
 
 if __name__ == "__main__":
-    create_path = os.getcwd() + "\\" + "project_file"
+    create_path = os.path.join(os.getcwd(), "project_file")
     add_file_name = "summarybook.docx"
-    print(create_path + "\\" + add_file_name)
+    title_name = "第一章/First Chapter"
+    chapter_name = "深度学习Python从入门到实践"
 
-    #路径存在与否判断
-    path_exist = os.path.exists(create_path)
-    if path_exist:
-        print("该目录存在")
-        print(path_exist)
-    else:
-        print("该目录不存在")
-        print(path_exist)
-        os.mkdir(create_path)  # 若不存在目录则先新建一个目录
+    first_chapter = Chapter(create_path, add_file_name, title_name, chapter_name)
+    first_chapter.createTitle()
 
-    file_exist = os.path.exists(create_path + "\\" + add_file_name)  # 判断当前文件路径是否存在，不存在也没事后面save会新建
-    if file_exist:
-        print("该文件存在")
-    else:
-        print("该文件不存在")
 
-    # 创建文件基本对象信息
-    document_object = Document()  # 创建文件对象
-    run_heading = document_object.add_heading("", level=1)
-    run = run_heading.add_run("第一章First Chapter")  # 以追加段落格式方便后续修改
-    run.font.underline = False  # 不需要添加下划线
-    run_heading.paragraph_format.space_before = Pt(0)  # 设置段前
-    run_heading.paragraph_format.space_after = Pt(0)  # 设置段后
-    run_heading.paragraph_format.line_spacing = 1.5  # 设置行间距
-    run.font.size = Pt(20)  # 设置1级标题文字的大小为“小四” 为12磅
-    run.font.name = "Times New Roman"  # 设置英文字体
-    run._element.rPr.rFonts.set(qn('w:eastAsia'), u"宋体")  # 设置中文字体
-    run.font.color.rgb = RGBColor(0, 0, 0)  # 设置颜色
 
-    # 创建内容
-    content = document_object.add_paragraph()
-    content.paragraph_format.space_before = Pt(10)  # 设置段前 0 磅
-    content.paragraph_format.space_after = Pt(10)  # 设置段后 0 磅
-    content.paragraph_format.line_spacing = 1.5  # 设置行间距为 1.5倍
-    content.paragraph_format.first_line_indent = Inches(0) #段落首行缩进为 0.5英寸
-    content.paragraph_format.first_line_indent = Inches(0)  # 相当于小四两个字符的缩进
-    content_run = content.add_run("深度学习Python从入门到实践")
-    content_run.bold = False  # 加粗
-    content_run.font.size = Pt(16)
-    content_run.font.name = "Times New Roman"  # 设置英文字体
-    content_run._element.rPr.rFonts.set(qn('w:eastAsia'), u"宋体")  # 设置中文字体
-    content_run.font.color.rgb = RGBColor(0, 0, 0)  # 设置颜色
-
-    # 创建更新时间，添加时间
-    header_section = document_object.sections[0]
-    header_pos = header_section.header
-    header_para = header_pos.paragraphs[0]
-    currentDateAndTime = datetime.now().strftime("%y-%m-%d %H:%M:%S")
-    header_para.text = "该章节编写于:" + str(currentDateAndTime)
-
-    document_object.save(create_path + "\\" + add_file_name)  # 创建文件保存地址
 
