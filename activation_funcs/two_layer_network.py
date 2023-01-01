caa8576d62fc0e2f0c4e3db3fe3762be5a81c104
Superdo <sijiexiongmore@outlook.com>
2023-01-01 14:17:04 +0800

    20230101:增加两层神经网络模型。

---
+++ activation_funcs/two_layer_network.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sigmoid_func import sigmoid
+from softmax_func import softmax
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        temp_val = x[idx]
+        x[idx] = temp_val + h
+        fxh1 = f(x)
+
+        x[idx] = temp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = temp_val
+    return grad
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params["W1"], self.params["W2"]
+        b1, b2 = self.params["b1"], self.params["b2"]
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy_var = np.sum(y == t) / float(x.shape[0])
+        return accuracy_var
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
+        grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
+        grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
+
+        return grads
+
+
+if __name__ == "__main__":
+    net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+
